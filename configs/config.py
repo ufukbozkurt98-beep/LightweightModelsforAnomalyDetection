c755e19db192568a718986903cfc8bee80d530c9
ufuk <ufukbozkurt@gmail.com>
2026-02-09 20:19:45 +0100

    project initialization from colab (Data extraction Data Check , pre-processing and data loader)

--- configs/config.py
+++ configs/config.py
@@ -12,10 +12,10 @@ IMAGE_INPUT_SIZE = 256
 SPLIT_JSON = REPORTS_DIR / f"mvtec_{CATEGORY}_split.json" # path object pointing to split file of the category
 NUM_WORKERS = 2
 TAR_PATH = Path("./data/mvtec_anomaly_detection.tar.xz")
-BACKBONE_KEY = "mobilenetv3_small"
+BACKBONE_KEY = "shufflenet_g3"
 #BACKBONE_KEY = "shufflenet_g1"  # ShuffleNet V1 g=1: baseline (144/288/576 ch)
 #BACKBONE_KEY = "shufflenet_g3"  # ShuffleNet V1 g=3: paper's default (240/480/960 ch)
 #BACKBONE_KEY = "shufflenet_g8"  # ShuffleNet V1 g=8: most aggressive (384/768/1536 ch)
-METHOD = "fastflow"
+METHOD = "cflow"
 #METHOD = "simplenet"
 #METHOD = "cflow"
