11c4c85217d6a367971dafdb40acc73c124d8e39
eylul007 <eylul.kose13@gmail.com>
2026-02-09 19:20:59 +0100

    project initialization from colab (Data extraction Data Check , pre-processing and data loader)

--- configs/config.py
+++ configs/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 MVTEC_ROOT = Path("./data/MVTec-AD")
 REPORTS_DIR = Path("./data/reports")
-CATEGORY = "cable"
+CATEGORY = "screw"
 VAL_RATIO = 0.2
 SEED = 42
 BATCH_SIZE = 16
@@ -11,7 +11,8 @@ IMAGE_INPUT_SIZE = 256
 SPLIT_JSON = REPORTS_DIR / f"mvtec_{CATEGORY}_split.json" # path object pointing to split file of the category
 NUM_WORKERS = 2
 TAR_PATH = Path("./data/mvtec_anomaly_detection.tar.xz")
-BACKBONE_KEY = "mobilenetv3_small"
+#BACKBONE_KEY = "mobilenetv3_small"
+BACKBONE_KEY = "efficientnet_lite0"
 METHOD = "glass"
 #METHOD = "simplenet"
 
