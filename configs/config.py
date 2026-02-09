4f0cb942fa1bf8a4693df6127d42a5b1bb774379
eylul007 <67484447+eylul007@users.noreply.github.com>
2026-02-10 00:17:35 +0100

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -3,17 +3,17 @@ from pathlib import Path
 
 MVTEC_ROOT = Path("./data/MVTec-AD")
 REPORTS_DIR = Path("./data/reports")
-CATEGORY = "screw"
+CATEGORY = "bottle"
 VAL_RATIO = 0.2
 SEED = 42
-BATCH_SIZE = 16
+BATCH_SIZE = 4
 IMAGE_INPUT_SIZE = 256
 SPLIT_JSON = REPORTS_DIR / f"mvtec_{CATEGORY}_split.json" # path object pointing to split file of the category
 NUM_WORKERS = 2
 TAR_PATH = Path("./data/mvtec_anomaly_detection.tar.xz")
 BACKBONE_KEY = "mobilenetv3_small"
 #BACKBONE_KEY = "efficientnet_lite0"
-METHOD = "glass"
-#METHOD = "simplenet"
+#METHOD = "glass"
+METHOD = "simplenet"
 
 
