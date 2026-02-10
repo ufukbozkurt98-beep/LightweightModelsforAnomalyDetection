7f2391b731955b23489f79dabe45d551102e0808
eylul007 <67484447+eylul007@users.noreply.github.com>
2026-02-10 03:48:04 +0100

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -6,14 +6,14 @@ REPORTS_DIR = Path("./data/reports")
 CATEGORY = "bottle"
 VAL_RATIO = 0.0
 SEED = 42
-BATCH_SIZE = 4
+BATCH_SIZE = 16
 IMAGE_INPUT_SIZE = 256
 SPLIT_JSON = REPORTS_DIR / f"mvtec_{CATEGORY}_split.json" # path object pointing to split file of the category
 NUM_WORKERS = 2
 TAR_PATH = Path("./data/mvtec_anomaly_detection.tar.xz")
-#BACKBONE_KEY = "mobilenetv3_small"
-BACKBONE_KEY = "efficientnet_lite0"
-#METHOD = "glass"
-METHOD = "simplenet"
+BACKBONE_KEY = "mobilenetv3_small"
+#BACKBONE_KEY = "efficientnet_lite0"
+METHOD = "glass"
+#METHOD = "simplenet"
 
 
