b03312a7cc617c27de3bbb5f67391ad72ae43255
ufuk <ufukbozkurt@gmail.com>
2026-03-09 00:54:21 +0100

    project initialization from colab (Data extraction Data Check , pre-processing and data loader)

--- configs/config.py
+++ configs/config.py
@@ -15,10 +15,10 @@ NUM_WORKERS = 4
 TAR_PATH = Path("./data/mvtec_anomaly_detection.tar.xz")
 DTD_ZIP_PATH = Path("./data/dtd-r1.0.1.tar.gz")  # DTD archive (tar.gz or zip)
 DTD_ROOT = Path("./data/dtd")  # extracted DTD root
-BACKBONE_KEY = "mobilenetv3_large"
+#BACKBONE_KEY = "mobilenetv3_large"
 #BACKBONE_KEY = "mobilenetv3_small"
 #BACKBONE_KEY = "mobilevit_s"
-#BACKBONE_KEY = "efficientnet_lite0"
+BACKBONE_KEY = "efficientnet_lite0"
 #BACKBONE_KEY = "shufflenet_g1"  # ShuffleNet V1 g=1: baseline (144/288/576 ch)
 #BACKBONE_KEY = "shufflenet_g3"  # ShuffleNet V1 g=3: paper's default (240/480/960 ch)
 #BACKBONE_KEY = "shufflenet_g8"  # ShuffleNet V1 g=8: most aggressive (384/768/1536 ch)
