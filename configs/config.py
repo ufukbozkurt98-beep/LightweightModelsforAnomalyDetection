73230e743601f19f1c173e8fecdb0009a71c2065
eylul007 <67484447+eylul007@users.noreply.github.com>
2026-04-06 01:20:56 +0200

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -20,8 +20,8 @@ DTD_ROOT = Path("./data/dtd")  # extracted DTD root
 #BACKBONE_KEY = "mobilenetv3_large"  # active backbone
 #BACKBONE_KEY = "mobilenetv3_small"
 #BACKBONE_KEY = "shufflenet_g3"
-BACKBONE_KEY = "mobilevit_xs"
-#BACKBONE_KEY = "efficientnet_lite1"
+#BACKBONE_KEY = "mobilevit_xs"
+BACKBONE_KEY = "efficientnet_lite1"
 #BACKBONE_KEY = "shufflenet_g1"  # ShuffleNet V1 g=1: baseline (144/288/576 ch)
 #BACKBONE_KEY = "shufflenet_g3"  # ShuffleNet V1 g=3: paper's default (240/480/960 ch)
 #BACKBONE_KEY = "shufflenet_g8"  # ShuffleNet V1 g=8: most aggressive (384/768/1536 ch)
@@ -29,6 +29,6 @@ BACKBONE_KEY = "mobilevit_xs"
 #BACKBONE_KEY = "mobileformer_294m"  # MobileFormer 294M FLOPs (11.4M params)
 #BACKBONE_KEY = "mobileformer_52m"   # MobileFormer 52M FLOPs (3.5M params)
 #METHOD = "fastflow"
-#METHOD = "simplenet"
-METHOD = "stlm"
-RUN_ALL = True
+METHOD = "simplenet"
+#METHOD = "stlm"
+RUN_ALL = False
